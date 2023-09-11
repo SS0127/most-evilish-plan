8d7677be917e8f5c3191d060da3aeff89d46d117
SS0127 <shinyleonel@gmail.com>
2023-09-11 17:00:58 +0500

    added nempi file

---
+++ nempi.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+ int yourAge;
+ int requiredAge=18;
+
+cout << "Enter your age UwU:";
+cin >> yourAge;
+if(yourAge>=requiredAge) {
+    cout << "Congratulations you can apply to this program! \n";
+    cout << "You have successfully applied to be sacrificed on next Eid-Ul-Adha";
+    }
+else {
+    cout << "sadge you can't apply because you don't have all the requirements :( ";
+
+}
+ return 0; }
